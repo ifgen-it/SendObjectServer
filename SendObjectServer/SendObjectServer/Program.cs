c4cbcec0076ed30d1a2ce454232dd74488966f74
Evgen Smirnov <ifgen.it@gmail.com>
2019-02-14 21:13:40 +0300

    created server

---
+++ SendObjectServer/SendObjectServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendObjectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("server here..");
+
+
+
+            Console.Read();
+        }
+    }
+}
