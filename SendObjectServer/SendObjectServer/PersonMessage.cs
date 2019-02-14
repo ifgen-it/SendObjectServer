d7e85ac97b748977b8df92d7f73a4563cdbd1c99
Evgen Smirnov <ifgen.it@gmail.com>
2019-02-14 21:34:44 +0300

    add main body

---
+++ SendObjectServer/SendObjectServer/PersonMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendObjectServer
+{
+    [Serializable]
+    public class PersonMessage
+    {
+        public int id;
+        public string userName;
+        public string message;
+        public List<string> tags;
+        public byte[] data;
+
+        public PersonMessage()
+        {
+            id = 0;
+            userName = null;
+            message = null;
+            tags = null;
+            data = null;
+        }
+
+        public PersonMessage(int ID, string UserName, string Message, List<string> Tags, byte[] Data)
+        {
+            id = ID;
+            userName = UserName;
+            message = Message;
+            tags = Tags;
+            data = Data;
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            res += "id: " + id + "\n";
+            res += "name: " + userName + "\n";
+            res += "message: " + message + "\n";
+            res += "tags: ";
+            for (int i = 0; i < tags.Count; i++)
+            {
+                res += "#" + tags[i] + " ";
+            }
+            return res;
+        }
+    }
+
+}
--- SendObjectServer/SendObjectServer/Program.cs
+++ SendObjectServer/SendObjectServer/Program.cs
@@ -2,18 +2,128 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace SendObjectServer
 {
     class Program
     {
+        static int port = 8005;
+        static string localHost = "127.0.0.1";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("server here..");
 
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(localHost), port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(10);
+                Console.WriteLine("Server was started. Waiting for connections...");
 
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
 
-            Console.Read();
+                    // GET THE MESSAGE
+                    int bytes = 0;
+                    byte[] data = new byte[2256];
+                    List<byte> reciever = new List<byte>();
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        byte[] buffer = null;
+                        if (bytes > 0)
+                        {
+                            buffer = new byte[bytes];
+                            for (int i = 0; i < bytes; i++)
+                            {
+                                buffer[i] = data[i];
+                            }
+                        }
+                        reciever.AddRange(buffer);
+                    } while (handler.Available > 0);
+
+                    byte[] answer = reciever.ToArray();
+
+                    PersonMessage p1 = XMLByteArrayToPersonMessage(answer);
+                    Console.WriteLine("\nDeserialized person message:\n\n" + p1.ToString());
+                    byte[] foto = p1.data;
+                    File.WriteAllBytes("newPic.jpg", foto);
+
+                    //Console.WriteLine(DateTime.Now.ToShortDateString() + ": " + builder.ToString());
+
+                    // SEND THE ANSWER
+                    string message = "your message delivered";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
         }
+
+
+        public static byte[] ObjectToByteArray(Object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        public static Object ByteArrayToObject(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return obj;
+            }
+        }
+
+        public static byte[] PersonMessageToXMLByteArray(PersonMessage obj)
+        {
+            XmlSerializer xmlSer = new XmlSerializer(typeof(PersonMessage));
+            using (var ms = new MemoryStream())
+            {
+                xmlSer.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+
+        }
+
+        public static PersonMessage XMLByteArrayToPersonMessage(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                XmlSerializer xmlSer = new XmlSerializer(typeof(PersonMessage));
+
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                PersonMessage person = (PersonMessage)xmlSer.Deserialize(memStream);
+
+                return person;
+            }
+        }
+
+
     }
 }
